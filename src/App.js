eb38f9d47f697ca0b401e2455b7e86eef17b9008
Geordy Decena <geordydecena@MacBook-Pro-3.local>
2020-11-06 18:25:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>Hi</div>
+  );
+}
+
+export default App;
